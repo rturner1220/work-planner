a1d938a1df113abf951621dc00a5aab0821e3d95
Rosa Turner <rmturner1220@gmail.com>
2022-05-29 15:41:53 -0400

    open javascript

---
+++ assets/planner.js
@@ -0,0 +1,27 @@
+var workPlanner = {};
+
+var workPlanner = function (workText, workTime, workDay)
+
+// daily planner to create a schedule
+
+
+// open the planner
+
+
+// current day is displayed on top / scrow down
+
+
+// present with time blocks and view the time block for that day
+
+
+// past, present and future separed by colors
+
+
+// click into a time block and enter and event
+
+
+// click to save button and save in local storage
+
+
+
+
