25d868243a40f28d8e360d7260d5b2eb0c036d66
Rosa Turner <rmturner1220@gmail.com>
2022-05-30 16:32:00 -0400

    adding time blocks and save button

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,30 +1,86 @@
-var workPlanner = {};
-
 // current day is displayed on top of planner
 function headerDay() {
-  var headerDay = moment().format('ddd, MMM Do');
+  var headerDay = moment().format('dddd, MMM Do');
   $("#currentDay").text(headerDay);
 }
 headerDay();
 
 // present with time blocks and view the time block for that day
 
+var myHours = [
+  {
+    id: "0",
+    hour : "09",
+    time: "09",
+    cenit: "am",
+    reminder: ""
+  },
+  {
+  id: "0",
+  hour : "09",
+  time: "09",
+  cenit: "am",
+  reminder: ""
+  },
+  {
+    id: "1",
+    hour : "10",
+    time: "10",
+    cenit: "am",
+    reminder: ""
+    },
+    {
+    id: "2",
+    hour : "11",
+    time: "11",
+    cenit: "am",
+    reminder: ""
+    },
+  ]
 
-// open the planner
+ myHours.forEach(function(thisHour) {
+   // creates timeblocks row
+   var timeblocks = $("<form>").attr({
+     "class": "row"
+   });
+      $(".container").append(timeblocks);
 
+   // creates time field
+   var hourBlock = $("<div>")
+   .text(`${thisHour.hour}${thisHour.cenit}`)
+   .attr({
+     "class": "col-md-2 hour"
+   });
+   
+   var hourplanner = $("<div>")
+   .attr({
+     "class": "col-md-9 description p-0"
+   });
 
-moment(testDate,'mm/dd/yyyy');
-moment(testDate).format('MM/DD/YYYY');
+   var planData = $("<textarea>");
+   hourplanner.append(planData);
+   planData.attr("id", thisHour.id);
+   if (thisHour.time < moment().format("HH")) {
+     planData.attr ({
+       "class": "past",
+     })
+   } else if (thisHour.time === moment().format("HH")) {
+     planData.attr ({
+       "class": "present"
+     })
+   } else if (thisHour.time > moment().format("HH")) {
+     planData.attr({
+       "class": "future"
+     })
+    }
 
+    // create save button
+   var saveBtn = $("<i class='far fa-save fa-lg'></i>")
+   var savePlanner = $("<button>")
+   .attr({
+     "class": "col-md-1 saveBtn"
+   });
 
-    
-
-// past, present and future separed by colors
-
-
-// click into a time block and enter and event
-
-
-// click to save button and save in local storage
-
-
+   savePlanner.append(saveBtn);
+   timeblocks.append(hourBlock, hourplanner, savePlanner);
+ })
