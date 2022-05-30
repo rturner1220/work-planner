b3a5444b90bf5e8bebc3e0d7ea4a06ace4b8ef55
Rosa Turner <rmturner1220@gmail.com>
2022-05-30 10:19:19 -0400

    diplay current day

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,18 +1,11 @@
 var workPlanner = {};
 
 // current day is displayed on top of planner
-// First we use the Date function, and then we process it into year, month and day, 
-//and display it in the way we want.
-var d = new Date()
-var year = d.getFullYear()
-var month = d.getMonth()+1;
-var day = d.getDate()
-
-var totalDays = new Date(year, month, 0).getDate()
-var printOut =
-(month<10 ? '0' : '') + month + '/' + 
-(day<10 ? '0' : '') + day + '/' + year
-$('.date').text(`${printOut}`)
+function headerDay() {
+  var headerDay = moment().format('ddd, MMM Do');
+  $("#currentDay").text(headerDay);
+}
+headerDay();
 
 // present with time blocks and view the time block for that day
 
@@ -20,6 +13,8 @@ $('.date').text(`${printOut}`)
 // open the planner
 
 
+moment(testDate,'mm/dd/yyyy');
+moment(testDate).format('MM/DD/YYYY');
 
 
     
