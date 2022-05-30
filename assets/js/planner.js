74bebf951d0f3334cdb429ced644b6ae8beb011b
Rosa Turner <rmturner1220@gmail.com>
2022-05-30 16:55:29 -0400

    adding time blocks and save button

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -8,35 +8,70 @@ headerDay();
 // present with time blocks and view the time block for that day
 
 var myHours = [
-  {
+    {
     id: "0",
     hour : "09",
     time: "09",
-    cenit: "am",
+    cenit: "AM",
     reminder: ""
-  },
-  {
-  id: "0",
-  hour : "09",
-  time: "09",
-  cenit: "am",
-  reminder: ""
-  },
-  {
+    },
+    {
     id: "1",
     hour : "10",
     time: "10",
-    cenit: "am",
+    cenit: "AM",
     reminder: ""
     },
     {
     id: "2",
     hour : "11",
     time: "11",
-    cenit: "am",
+    cenit: "AM",
     reminder: ""
     },
-  ]
+    {
+      id: "3",
+      hour : "12",
+      time: "12",
+      cenit: "PM",
+      reminder: ""
+    },
+    {
+      id: "4",
+      hour : "1",
+      time: "1",
+      cenit: "PM",
+      reminder: ""
+    },
+    {
+      id: "5",
+      hour : "2",
+      time: "2",
+      cenit: "PM",
+      reminder: ""
+    },
+    {
+      id: "6",
+      hour : "3",
+      time: "3",
+      cenit: "PM",
+      reminder: ""
+    },
+    {
+      id: "7",
+      hour : "4",
+      time: "4",
+      cenit: "PM",
+      reminder: ""
+    },
+    {
+      id: "8",
+      hour : "5",
+      time: "5",
+      cenit: "PM",
+      reminder: ""
+    },
+]
 
  myHours.forEach(function(thisHour) {
    // creates timeblocks row
@@ -57,6 +92,7 @@ var myHours = [
      "class": "col-md-9 description p-0"
    });
 
+   // time block is color-coded in past, present, or future
    var planData = $("<textarea>");
    hourplanner.append(planData);
    planData.attr("id", thisHour.id);
