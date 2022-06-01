0f1a4da49addd13f98423aadcec8d3a626000f4a
Rosa Turner <rmturner1220@gmail.com>
2022-06-01 18:45:58 -0400

    save in localstorage

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -9,123 +9,153 @@ headerDay();
 
 var myHours = [
     {
-    id: "0",
-    hour : "09",
-    time: "09",
-    cenit: "AM",
-    reminder: ""
+    id: '0',
+    hour : '09',
+    time: '09',
+    cenit: 'AM',
+    reminder: '',
     },
     {
-    id: "1",
-    hour : "10",
-    time: "10",
-    cenit: "AM",
-    reminder: ""
+    id: '1',
+    hour : '10',
+    time: '10',
+    cenit: 'AM',
+    reminder: '',
     },
     {
-    id: "2",
-    hour : "11",
-    time: "11",
-    cenit: "AM",
-    reminder: ""
+    id: '2',
+    hour : '11',
+    time: '11',
+    cenit: 'AM',
+    reminder: '',
     },
     {
-      id: "3",
-      hour : "12",
-      time: "12",
-      cenit: "PM",
-      reminder: ""
+      id: '3',
+      hour : '12',
+      time: '12',
+      cenit: 'PM',
+      reminder: '',
     },
     {
-      id: "4",
-      hour : "1",
-      time: "1",
-      cenit: "PM",
-      reminder: ""
+      id: '4',
+      hour : '1',
+      time: '13',
+      cenit: 'PM',
+      reminder: '',
     },
     {
-      id: "5",
-      hour : "2",
-      time: "2",
-      cenit: "PM",
-      reminder: ""
+      id: '5',
+      hour : '2',
+      time: '14',
+      cenit: 'PM',
+      reminder: '',
     },
     {
-      id: "6",
-      hour : "3",
-      time: "3",
-      cenit: "PM",
-      reminder: ""
+      id: '6',
+      hour : '3',
+      time: '15',
+      cenit: 'PM',
+      reminder: '',
     },
     {
-      id: "7",
-      hour : "4",
-      time: "4",
-      cenit: "PM",
-      reminder: ""
+      id: '7',
+      hour : '4',
+      time: '16',
+      cenit: 'PM',
+      reminder: '',
     },
     {
-      id: "8",
-      hour : "5",
-      time: "5",
-      cenit: "PM",
-      reminder: ""
+      id: '8',
+      hour : '5',
+      time: '17',
+      cenit: 'PM',
+      reminder: '',
     },
-]
-
- myHours.forEach(function(thisHour) {
-   // creates time blocks row
-   var timeBlocks = $("<form>").attr({
-     "class": "row"
-   });
-      $(".container").append(timeBlocks);
-
-   // creates time block
-   var hourBlock = $("<div>")
-   .text(`${thisHour.hour}${thisHour.cenit}`)
-   .attr({
-     "class": "col-md-2 hour"
-   });
+];
    
-   // create schedule data
-   var hourPlan = $("<div>")
-   .attr({
-     "class": "col-md-9 description p-0"
-   });
+// creates time block
+function buildTimeBlock(thisHour) {
+  var timeBlock = $('<form>').attr({
+    'class': 'row',
+    'data-index': thisHour.id,
+  });
 
-   // time block is color-coded in past, present, or future
-   var planData = $("<textarea>");
-   hourPlan.append(planData);
-   planData.attr("id", thisHour.id);
-   if (thisHour.time < moment().format('HH')) {
+  timeBlock.append(buildHourBlock(thisHour), buildHourPlan(thisHour), buildSavePlan());
+    return timeBlock;  
+}
+
+function buildHourBlock(thisHour) {
+  var hourBlock = $('<div>')
+  .text(`${thisHour.hour}${thisHour.cenit}`)
+  .attr({
+    'class': 'col-md-2 hour',
+  });
+
+    return hourBlock;
+}
+   
+// create schedule data
+function buildPlanData(thisHour) {
+  var now = moment();
+  var planData = $('<textarea>');
+   planData.attr('id', thisHour.id);
+
+  // time block is color-coded in past, present, or future
+   if (thisHour.time < now.format('HH')) {
      planData.attr ({
-       "class": "past",
-     })
-   } else if (thisHour.time === moment().format('HH')) {
+       'class': 'past',
+     });
+
+   } else if (thisHour.time === now.format('HH')) {
      planData.attr ({
-       "class": "present"
-     })
-   } else if (thisHour.time > moment().format('HH')) {
+       'class': 'present',
+     });
+   } else if (thisHour.time > now.format('HH')) {
      planData.attr({
-       "class": "future"
-     })
+       'class': 'future'
+     });
     }
 
-    // text for the event is saved in local storage
-    function saveReminders() {
-      localStorage.setItem("myHours", JSON.stringify(myHours));
-    }
+    return planData;
+}
+
+function buildHourPlan(thisHour) {
+  var hourPlan = $("<div>")
+  .attr({
+    'class': 'col-md-9 description p-0',
+  });
+
+  hourPlan.append(buildPlanData(thisHour));
+
+    return hourPlan;
+}
+
+function buildSavePlan() {
+  var saveButton = $('<i class=\'far fa-save fa-lg\'></i>');
+  var savePlan = $('<button>')
+  .attr({
+    'class': 'col-md-1 saveBtn',
+  });
+  savePlan.append(saveButton);
+
+    return savePlan;
+}
+
+// text for the event is saved in local storage
+function saveReminders() {
+  localStorage.setItem("myHours", JSON.stringify(myHours));
+}
   
-  // sets data in localstorage
-    function displayReminders() {
-      myHours.forEach(function (_thisHour) {
-        $(`#${_thisHour.id}`).val(_thisHour.reminder);
-      })
-    }
+// sets data in localstorage
+function displayReminders() {
+  myHours.forEach(function (_thisHour) {
+    $(`#${_thisHour.id}`).val(_thisHour.reminder);
+    });
+  }
 
-  // sets existing local storage data to the view if it exists 
+// sets existing local storage data to the view if it exists 
     function start() {
-    var storedDay = JSON.parse(localStorage.getItem("myHours"));
+    var storedDay = JSON.parse(localStorage.getItem('myHours'));
     
     if (storedDay) {
       myHours = storedDay;
@@ -134,24 +164,24 @@ var myHours = [
     saveReminders();
     displayReminders();
   }
+
+  myHours.forEach(function (thisHour) {
+    $('.container').append(buildTimeBlock(thisHour));
+
     start();
+  });
 
-    // create save btn
-    var saveButton = $("<i class='far fa-save fa-lg'></i>")
-    var savePlan = $("<button>")
-    .attr({
-      "class": "col-md-1 saveBtn"
-    });
-    savePlan.append(saveButton);
-    timeBlocks.append(hourBlock, hourPlan, savePlan);
-  })
-
-  // save data in localstorage
-  $(".saveBtn").on("click", function(event) {
+// save data in localstorage
+  $('.saveBtn').on('click', function(event) {
     event.preventDefault();
-    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
-    myHours[saveIndex].reminder = $(this).siblings(".description").children(".future").val();
+
+    var row = $(this).closest('.row');
+    var saveIndex = row.data('index');
+    myHours[saveIndex].reminder = row.find('textarea').val();
+
     console.log(saveIndex);
     saveReminders();
     displayReminders();
-})
+});   
+
+  
